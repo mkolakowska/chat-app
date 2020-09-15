fe900e43eb9d0279445d87909a059ee27be0f280
Malgorzata Kolakowska <m.a.kolakowska@googlemail.com>
2020-09-15 10:37:05 +0100

    Added Chat Bot.

--- client/app.js
+++ client/app.js
@@ -1,57 +1,75 @@
-'use strict';
-{
-  const loginForm = document.getElementById('welcome-form');
-  const messageSection = document.getElementById('messages-section');
-  const messagesList = document.getElementById('messages-list');
-  const addMessageForm = document.getElementById('add-messages-form');
-  const userNameInput = document.getElementById('username');
-  const messageContentInput = document.getElementById('message-content');
-  let userName = '';
+// Reference
 
-  function login(e) {
-    e.preventDefault();
+const loginForm = document.getElementById('welcome-form');
+const messageSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
 
-    if (userNameInput.value == '' || userNameInput.value == undefined) {
-      alert('Please type the user name!');
-    } else {
-      userName = userNameInput.value;
-      loginForm.classList.remove('show');
-      messageSection.classList.add('show');
-    }
+const socket = io();
+
+let userName = '';
+
+function login(e) {
+  e.preventDefault();
+
+  if (!userNameInput.value) {
+    alert('Please type the user name!');
+  } else {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messageSection.classList.add('show');
+    socket.emit('join', userName);
   }
+}
 
-  function sendMessage(e) {
-    e.preventDefault();
-
-    if (
-      messageContentInput.value == '' ||
-      messageContentInput.value == undefined
-    ) {
-      alert('Please type the message!');
-    } else {
-      addMessage(userName, messageContentInput.value);
-    }
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if (author === userName) {
+    message.classList.add('message--self');
+  } else if (author === 'Chat Bot') {
+    message.classList.add('message--chatbot');
   }
-
-  function addMessage(author, content) {
-    const message = document.createElement('li');
-    message.classList.add('message');
-    message.classList.add('message--received');
-    if (author === userName) message.classList.add('message--self');
-    message.innerHTML = `
+  message.innerHTML = `
       <h3 class="message__author">${userName === author ? 'You' : author}</h3>
       <div class="message__content">
         ${content}
       </div>
     `;
-    messagesList.appendChild(message);
-  }
-
-  loginForm.addEventListener('submit', (e) => {
-    login(e);
-  });
-
-  addMessageForm.addEventListener('submit', (e) => {
-    sendMessage(e);
-  });
+  messagesList.appendChild(message);
 }
+
+function sendMessage(e) {
+  e.preventDefault();
+
+  let messageContent = messageContentInput.value;
+
+  if (!messageContent.length) {
+    alert('Please type the message!');
+  } else {
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent });
+    messageContentInput.value = '';
+  }
+}
+
+loginForm.addEventListener('submit', (e) => {
+  login(e);
+});
+
+addMessageForm.addEventListener('submit', (e) => {
+  sendMessage(e);
+});
+
+socket.on('message', ({ author, content }) => addMessage(author, content));
+
+socket.on('newUser', (userName) => {
+  addMessage('Chat Bot', `${userName} has joined the conversation!`);
+});
+
+socket.on('removedUser', ({ name }) => {
+  addMessage('Chat Bot', `${name} has left the conversation... :(`);
+});
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 
 const messages = [];
+const users = [];
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/')));
@@ -17,6 +19,28 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', (message) => {
+    console.log("Oh, I've got something from " + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('join', (userName) => {
+    users.push({ name: userName, id: socket.id });
+    socket.broadcast.emit('newUser', userName);
+  });
+  socket.on('disconnect', () => {
+    console.log('Oh, socket ' + socket.id + ' has left');
+    const index = users.findIndex((item) => item.id === socket.id);
+    const user = users[index];
+    socket.broadcast.emit('removedUser', user);
+    users.splice(index, 1);
+  });
+  console.log("I've added a listener on message and disconnect events \n");
 });
